a6026fc37cb86bd0c579e7f739e45cd454f36e3c
Basile Vernouillet <basile64.v@gmail.com>
2020-11-11 16:06:30 +0100

    re-arrange code

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
+const path = require('path');
 const express = require('express');
-const app = express();
 const cors = require('cors');
 const helmet = require('helmet');
 const morgan = require('morgan');
-const path = require('path');
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+const games = require('./api.json');
 
 const { errorHandler, notFound } = require('./middlewares');
-const games = require('./api.json');
-const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(helmet());
